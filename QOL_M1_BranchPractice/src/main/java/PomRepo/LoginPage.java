90497d5c692875e95952340a368c7c87c84b93ea
smurt <smurt@DESKTOP-QARHQMJ>
2024-01-14 12:00:10 +0530

    Changes made,please check

--- QOL_M1_BranchPractice/src/main/java/PomRepo/LoginPage.java
+++ QOL_M1_BranchPractice/src/main/java/PomRepo/LoginPage.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.support.FindBy;
 public class LoginPage {
 	@FindBy(id="Email")
     private WebElement emailTextField;
-	@FindBy(id="password")
+	@FindBy(xpath="password")
     private WebElement passwordTextField;
 	@FindBy(xpath = "//input[@value='Log in']")
 	private WebElement loginButton;
